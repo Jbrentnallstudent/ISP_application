fd2310828b3711b8c63c6cd3a1cead17d12a8111
Jbrentnallstudent <josh.david1206@gmail.com>
2024-12-05 16:23:48 -0330

    New commit

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,12 +3,6 @@ plugins {
     alias(libs.plugins.android.application) apply false
 }
 
-dependencies {
-    implementation 'androidx.navigation:navigation-fragment-ktx:2.7.2'
-    implementation 'androidx.navigation:navigation-ui-ktx:2.7.2'
-    implementation 'com.prolificinteractive:material-calendarview:1.4.3'
-    implementation 'androidx.room:room-runtime:2.5.0'
-    annotationProcessor 'androidx.room:room-compiler:2.5.0' // or use kapt if using Kotlin
-}
+
 
 
