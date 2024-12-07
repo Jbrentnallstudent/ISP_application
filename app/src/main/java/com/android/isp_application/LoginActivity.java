d9e0e144be120acc184e1c08a786e5336dbf9ed5
Jbrentnallstudent <josh.david1206@gmail.com>
2024-12-07 14:33:23 -0330

    New commit

--- app/src/main/java/com/android/isp_application/LoginActivity.java
+++ app/src/main/java/com/android/isp_application/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.android.isp_application;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -33,10 +35,20 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View v) {
                 String username = edUsername.getText().toString();
                 String password = edPassword.getText().toString();
+                Database db = new Database(getApplicationContext(),"Student Registry",null,1);
                 if (username.length()==0 || password.length()==0) {
-                    Toast.makeText(getApplicationContext(), "Invalid email or password", Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), "Please fill in all details", Toast.LENGTH_SHORT).show();
                 }else {
-                    Toast.makeText(getApplicationContext(), "Login Success", Toast.LENGTH_SHORT).show();
+                    if (db.login(username,password)==1){
+                        Toast.makeText(getApplicationContext(), "Login Success", Toast.LENGTH_SHORT).show();
+                        SharedPreferences sharedpreferences = getSharedPreferences("shared_prefs", Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedpreferences.edit();
+                        editor.putString("username", username);
+                        editor.apply();
+                        startActivity(new Intent(LoginActivity.this,HomeActivity.class));
+                    }else{
+                        Toast.makeText(getApplicationContext(), "Invalid email or password", Toast.LENGTH_SHORT).show();
+                    }
                 }
             }
         });
